8cdb355aa1358febcf25dd099ad5ffb7a28ee9b3
jwyxym <jwyxym@126.com>
2025-08-10 22:30:32 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use anyhow::{Result};
-use fantoccini::{
-    Client, Locator,
-    elements::{
-        Element
-    }
-};
+use reqwest::{self};
 use urlencoding::{encode};
 use std::{
-    thread,
-    time::Duration,
     fs::{
-        File
+        File,
+        OpenOptions,
+        read_to_string
+    },
+    io::{
+        Write,
+        LineWriter,
     }
 };
 use serde::Serialize;
+use serde_json::{Value};
+use chrono::{Local, DateTime};
 
 #[derive(Serialize)]
 struct Word {
@@ -36,61 +37,149 @@ impl Word {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    const MAX_LEN: usize = 20000;
-    let client: Client = Client::new("http://localhost:9515").await?;
-    let mut words: Vec<Word> = vec![Word::new("卷甲衔枚")];
+    const MAX_LENGTH: usize = 100000;
+    let mut words: Vec<Word> = vec![Word::new("披麻戴孝")];
+    let mut remove: Vec<String> = vec![];
+    let file: File = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("idiom.json")?;
+    let mut writer: LineWriter<File>= LineWriter::new(file);
+    let file: File = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("log.txt")?;
+    let mut txt_writer: LineWriter<File>= LineWriter::new(file);
     loop {
-        print!("loop...\n");
-        let mut remove: Vec<String> = vec![];
+        println!("loop...");
+        println!("{}", words.len());
+        println!("{}", remove.len());
+        writeln!(&mut txt_writer, "{}", words.len())?;
+        for word in words.iter().rev().take(10) {
+            writeln!(&mut txt_writer, "{}", word.word)?;
+        }
+        write!(&mut txt_writer, "\n")?;
+        let mut cache: Vec<Word> = vec![];
         for ct in 0..words.len() {
-            if words[ct].pinyin != "" {
+            let content: String = read_to_string("idiom.json")?;
+            if words[ct].pinyin != "" || content.contains(&format!("\"word\":\"{}\"", &words[ct].word)) {
+                remove.push(words[ct].word.clone());
                 continue;
             }
-            let url: String = format!("https://hanyu.baidu.com/hanyu-page/term/detail?wd={}&ptype=zici", encode(&words[ct].word));
-            client.goto(&url).await?;
-            thread::sleep(Duration::from_millis(200));
-            match client.find(Locator::Css(".idiom-container")).await {
-                Ok(idiom_container) => {
-                    let body: Element = client.find(Locator::Css(".term-container")).await?;
-                    let pinyin_box: Element = body.find(Locator::Css(".pinyin-box")).await?;
-                    let word_name: String = pinyin_box.find(Locator::Css(".name")).await?.text().await?;
-                    let word_pinyin: String = pinyin_box.find(Locator::Css(".pinyin-item")).await?.text().await?.replace("[", "").replace("]", "");
-                    let idioms:Vec<Element> = idiom_container.find_all(Locator::Css(".idiom-wrap")).await?;
-                    let mut word_explanation: String = "".to_string();
+            let url: String = format!("https://hanyuapp.baidu.com/dictapp/swan/termdetail?wd={}&client=pc&source_tag=2&lesson_from=xiaodu", encode(&words[ct].word));
+            match reqwest::get(url).await {
+                Ok(res) => {
+                    let response: Value = res.json().await?;
                     let mut word_derivation: String = "".to_string();
-                    for i in idioms.iter() {
-                        let title: String = i.find(Locator::Css(".idiom-title")).await?.text().await?;
-                        if word_explanation == "".to_string() && title.contains("基本释义") {
-                            word_explanation = i.find(Locator::Css(".item-right")).await?.text().await?;
+                    let __: Option<()> = match response["data"]["chuChu"].as_array() {
+                        Some(chu_chu) if !chu_chu.is_empty() => {
+                            if let Some(item) = chu_chu[0].as_object() {
+                                if item["citeOriginalText"].as_str().unwrap_or("") != "" {
+                                    word_derivation = format!(
+                                        "{} ——{}·{}·{}", 
+                                        item["citeOriginalText"].as_str().unwrap_or(""),
+                                        item["dynasty"].as_str().unwrap_or(""),
+                                        item["author"].as_str().unwrap_or(""),
+                                        item["source"].as_str().unwrap_or("")
+                                    );
+                                }
+                            }
+                            Some(())
+                        },
+                        Some(_) => None,
+                        None => None
+                    };
+                    let mut word_explanation: String = "".to_string();
+                    let __: Option<()> = match response.get("data").and_then(|data| data.get("definitionInfo")) {
+                        Some(definition) => {
+                            word_explanation = format!("{}", &definition["definition"]);
+                            word_explanation = word_explanation.replace("\"", "");
+                            Some(())
+                        },
+                        None => { None }
+                    };
+                    let mut word_pinyin: String = "".to_string();
+                    let __: Option<()> = match response.get("data").and_then(|data| data.get("pinyin")) {
+                        Some(pinyin) => {
+                            word_pinyin = format!("{}", &pinyin);
+                            word_pinyin = word_pinyin.replace("\"", "");
+                            Some(())
+                        },
+                        None => { None }
+                    };
+                    if !content.contains(&words[ct].word) {
+                        let now: DateTime<Local> = Local::now();
+                        words[ct].pinyin = word_pinyin;
+                        words[ct].explanation = word_explanation;
+                        words[ct].derivation = word_derivation;
+                        if words[ct].pinyin != "".to_string() || words[ct].explanation != "".to_string() {
+                            println!("[{}]{} {} {} {}", now.format("%Y-%m-%d %H:%M:%S"), &words[ct].derivation, &words[ct].explanation, &words[ct].pinyin, &words[ct].word);
+                            writeln!(&mut writer, "\t{},", serde_json::to_string(&words[ct])?)?;
                         }
-                        if word_derivation == "".to_string() && title.contains("出") && title.contains("处") {
-                            word_derivation = format!("{} ——{}", i.find(Locator::Css(".item-text")).await?.text().await?, i.find(Locator::Css(".item-source")).await?.text().await?);
-                        }
+                        remove.push(words[ct].word.clone());
                     }
-                    println!("{} {} {} {}", &word_derivation, &word_explanation, &word_pinyin, &word_name);
-                    words[ct].pinyin = word_pinyin;
-                    words[ct].explanation = word_explanation;
-                    words[ct].derivation = word_derivation;
-                    let relate_item: Vec<Element> = body.find_all(Locator::Css(".relate-item")).await?;
-                    for i in relate_item.iter() {
-                        let text: String = i.find(Locator::Css(".text")).await?.text().await?;
-                        if words.iter().all(|x: &Word| x.word != text) && words.len() < MAX_LEN {
-                            words.push(Word::new(&text));
-                        }
-                    }
+                    let __: Option<()> = match response["data"]["relationInfo"]["relationList"].as_array() {
+                        Some(relations) if !relations.is_empty() => {
+                            for relation in relations {
+                                if let Some(obj) = relation.as_object() && obj["name"].as_str().unwrap_or("") != "" {
+                                    let text: &str= obj["name"].as_str().unwrap_or("");
+                                    let content: String = read_to_string("idiom.json")?;
+                                    if !content.contains(&format!("\"word\":\"{}\"", &text)) {
+                                        cache.push(Word::new(&text));
+                                    }
+                                }
+                            }
+                            Some(())
+                        },
+                        Some(_) => None,
+                        None => None
+                    };
+                    let __: Option<()> = match response["data"]["synonyms"].as_array() {
+                        Some(synonyms) if !synonyms.is_empty() => {
+                            for synonym in synonyms {
+                                if let Some(obj) = synonym.as_object() && obj["name"].as_str().unwrap_or("") != "" {
+                                    let text: &str= obj["name"].as_str().unwrap_or("");
+                                    let content: String = read_to_string("idiom.json")?;
+                                    if !content.contains(&format!("\"word\":\"{}\"", &text)) {
+                                        cache.push(Word::new(&text));
+                                    }
+                                }
+                            }
+                            Some(())
+                        },
+                        Some(_) => None,
+                        None => None
+                    };
+                    let __: Option<()> = match response["data"]["antonym"].as_array() {
+                        Some(antonyms) if !antonyms.is_empty() => {
+                            for antonym in antonyms {
+                                if let Some(obj) = antonym.as_object() && obj["name"].as_str().unwrap_or("") != "" {
+                                    let text: &str= obj["name"].as_str().unwrap_or("");
+                                    let content: String = read_to_string("idiom.json")?;
+                                    if !content.contains(&format!("\"word\":\"{}\"", &text)) {
+                                        cache.push(Word::new(&text));
+                                    }
+                                }
+                            }
+                            Some(())
+                        },
+                        Some(_) => None,
+                        None => None
+                    };
                 },
-                Err(_) => {
+                Err(e) => {
+                    println!("{}", e);
                     remove.push(words[ct].word.clone());
-                }
+                },
             }
         }
-        words.retain(|word: &Word| !remove.contains(&word.word));
-        if words.iter().all(|x: &Word| x.pinyin != "") {
+        let content: String = read_to_string("idiom.json")?;
+        words = cache;
+        words.retain(|word: &Word| !remove.contains(&word.word) && !content.contains(&format!("\"word\":\"{}\"", &word.word)));
+        if words.iter().all(|x: &Word| x.pinyin != "") || remove.len() > MAX_LENGTH {
             break;
         }
     }
-    client.close().await?;
-    let file = File::create("idiom.json")?;
-    serde_json::to_writer_pretty(file, &words)?;
+
     return Ok(())
 }
